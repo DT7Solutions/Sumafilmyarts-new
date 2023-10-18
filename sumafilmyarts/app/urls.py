ee23104dd55a985e7bff160aa24138d249575e0f
suresh522 <kajasuresh522@gmail.com>
2023-10-18 18:36:28 +0530

    layout designs done

--- sumafilmyarts/app/urls.py
+++ sumafilmyarts/app/urls.py
@@ -5,5 +5,14 @@ from app import views
 urlpatterns = [
    
     path('', views.home, name='home'),
+    path('about/',views.about, name='about'),
     path('portfolio/',views.portfolio, name='portfolio'),
+    path('collaborate/', views.collaborate, name='collaborate'),
+    path('sponsorship/',views.sponsorship, name='sponsorship'),
+    path('newsevents/',views.newsevents, name='newsevents'),
+    path('blog/', views.blog, name='blog'),
+    path('career/', views.career, name='career'),
+    path('enquiries/',views.enquiries, name='enquiries'),
+    path('youridea/', views.youridea, name='youridea'),
+    path('formapply/', views.formapply, name='formapply'),
 ]
--- sumafilmyarts/app/views.py
+++ sumafilmyarts/app/views.py
@@ -6,5 +6,32 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'uifiles/home.html')
 
+def about(request):
+    return render(request, 'uifiles/about-us.html')
+
 def portfolio(request):
     return render(request, 'uifiles/portfolio.html')
+
+def collaborate(request):
+    return render(request, 'uifiles/Collaborations.html')
+
+def sponsorship(request):
+    return render(request, 'uifiles/sponsorship.html')
+
+def newsevents(request):
+    return render(request, 'uifiles/newsandevents.html')
+
+def blog(request):
+    return render(request, 'uifiles/blogs.html')
+
+def career(request):
+    return render(request, 'uifiles/career.html')
+
+def enquiries(request):
+    return render(request, 'uifiles/enquiries.html')
+
+def youridea(request):
+    return render(request, 'uifiles/youridea.html')
+
+def formapply(request):
+    return render(request, 'uifiles/applyforjobform.html')
